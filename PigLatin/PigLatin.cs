709f04b56d71025e332b8c8f86faab1f2a2154e6
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-11-01 17:17:48 -0400

    Initial code push and final code. Totally not broken

---
+++ PigLatin/PigLatin.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PigLatin
+{
+    class PigLatin
+    {
+
+        public bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+
+            //return c.ToString() == vowels.ToString();
+            //Changed it to check and see if any of the vowels are what we passed in.
+            return vowels.Contains(c);
+        }
+
+        public bool IsSpecialChar(string word)
+        {
+            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+            word = word.ToLower();
+            //foreach (char c in specialChars)
+            //{
+            //    foreach (char w in word)
+            //    {
+            //        if (w == c)
+            //        {
+            //            Console.WriteLine("That word has special characters, we will return that section as is");
+            //            return true;
+            //        }
+            //    }
+            //}
+
+
+            //changed this to be more concise. No need for two for loops due to .Contains()
+            foreach(char w in word)
+            {
+                if (specialChars.Contains(w))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        public bool StringHasVowels(string word)
+        {
+            foreach (char letter in word)
+            {
+                if (IsVowel(letter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string ToPigLatin(string word)
+        {
+
+
+            if (IsSpecialChar(word))
+            {
+                return word;
+            }
+            else if (!StringHasVowels(word))
+            {
+                return word;
+            }
+
+            char firstLetter = word[0];
+            string output = "placeholder";
+            if (IsVowel(firstLetter) == true)
+            {
+                //added a missing w to this seciton so that words with first letter vowel get correct suffix
+                output = word + "way";
+            }
+            else
+            {
+                int vowelIndex = -1;
+                //Handle going through all the consonants
+                for (int i = 0; i <= word.Length; i++)
+                {
+                    if (IsVowel(word[i]) == true)
+                    {
+                        vowelIndex = i;
+                        break;
+                    }
+                }
+
+
+                //fixed these two indexes so that we can see if it's working correctly
+                string sub = word.Substring(vowelIndex);
+                string postFix = word.Substring(0, vowelIndex);
+
+                output = sub + postFix + "ay";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ PigLatin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace PigLatin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PigLatin pigLatin = new PigLatin();
+            string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
+            //Added this section to split  the string if it's multiple words so that the special characters clauuse doesn't automatically kick them out
+            string[] splitApart = userInput.Split(' ');
+            string translation = string.Empty;
+
+            foreach(string word in splitApart)
+            {
+                translation += pigLatin.ToPigLatin(word) + " ";
+            }
+
+            Console.WriteLine(translation);
+        }
+
+        public static string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine().ToLower().Trim();
+            return input;
+        }
+    }
+}
